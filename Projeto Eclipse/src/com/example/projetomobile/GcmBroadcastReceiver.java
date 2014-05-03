d9d618973f5a4a2d2c8f9fcf4c43fe546473c59c
paulo <paulo@>
2014-05-03 14:53:08 -0300

    Adicionada implementação do método "onReceive" da classe "GcmBroadcastReceiver"

--- Projeto Eclipse/src/com/example/projetomobile/GcmBroadcastReceiver.java
+++ Projeto Eclipse/src/com/example/projetomobile/GcmBroadcastReceiver.java
@@ -1,5 +1,7 @@
 package com.example.projetomobile;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
@@ -9,6 +11,11 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver{
 	@Override
 	public void onReceive(Context arg0, Intent arg1) {
 		
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        //Inicia o serviço, mantendo o dispositivo acordado enquanto está executando.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
 		
 	}
 	
