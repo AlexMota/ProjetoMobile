fa959822e86ccedcb829b02f9a35f6988bb6e713
paulohsg <paulohenrique_sg@hotmail.com>
2014-06-24 23:57:43 -0300

    Adição de bibliotecas

--- Projeto ADT/src/com/example/projetomobile/adapter/TabsPagerAdapter.java
+++ Projeto ADT/src/com/example/projetomobile/adapter/TabsPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.projetomobile.adapter;
 
 import android.app.ListFragment;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
